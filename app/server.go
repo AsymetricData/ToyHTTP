3473499eea08783f3199663e5608f562f8dcd87f
Cédric <ses.courteau.cedric@gmail.com>
2023-09-27 19:49:31 +0200

    Update model

--- app/server.go
+++ app/server.go
@@ -51,6 +51,10 @@ func handleRequest(conn net.Conn) {
 		value := r.Params["value"]
 		writeResponse("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: "+strconv.Itoa(len(value))+"\r\n\r\n"+value, 200, conn)
 	})
+	router.Handle("/echo/{value}/{other}", func(conn net.Conn, r *request.Request) {
+		value := r.Params["value"]
+		writeResponse("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: "+strconv.Itoa(len(value))+"\r\n\r\n"+value, 200, conn)
+	})
 
 	r := request.NewRequest(buffer)
 
