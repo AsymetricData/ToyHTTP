eede95a897b16b1a7e6049bcb5cb783a72185e45
Cédric <ses.courteau.cedric@gmail.com>
2023-09-27 19:50:42 +0200

    Update model

--- app/server.go
+++ app/server.go
@@ -52,7 +52,8 @@ func handleRequest(conn net.Conn) {
 		writeResponse("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: "+strconv.Itoa(len(value))+"\r\n\r\n"+value, 200, conn)
 	})
 	router.Handle("/echo/{value}/{other}", func(conn net.Conn, r *request.Request) {
-		writeResponse("HTTP/1.1 404 Not Found", 404, conn)
+		value := r.Params["value"]
+		writeResponse("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: "+strconv.Itoa(len(value))+"\r\n\r\n"+value, 200, conn)
 	})
 
 	r := request.NewRequest(buffer)
